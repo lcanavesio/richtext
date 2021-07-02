936d773479937892e3f1c33169632dff3acd202b
lcanavesio <lcanavesio@gmail.com>
2021-07-02 19:09:58 -0300

    richtext

---
+++ README.md
@@ -0,0 +1,15 @@
+# TinyMCE + NextJS
+
+## Install dependencies.
+
+```bash
+yarn
+```
+## Run wysiwyg.
+
+```bash
+yarn dev
+```
+
+## Contributing
+Pull requests are welcome.
---
+++ src/components/AppBarEditor.tsx
@@ -0,0 +1,43 @@
+import AppBar from "@material-ui/core/AppBar";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import React from "react";
+import CustomEditor from "./tinymce/CustomEditor";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  })
+);
+
+const AppBarEditor = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <div>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" className={classes.title}>
+              TinyMCE + NextJS
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </div>
+      <div>
+        <CustomEditor />
+      </div>
+    </div>
+  );
+};
+
+export default AppBarEditor;
